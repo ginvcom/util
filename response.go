b6de20d6f2cad60e37fb0c55ed5d6a4897f87dc2
luohancai <luohancai@taqu.cn>
2023-04-08 21:05:02 +0800

    feat: 增加error接口

--- response.go
+++ response.go
@@ -6,16 +6,37 @@ import (
 	"github.com/zeromicro/go-zero/rest/httpx"
 )
 
+type ErrorMsg struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+}
+
+func (e *ErrorMsg) Error() string {
+	return e.Msg
+}
+
 type Body struct {
 	Code int         `json:"code"`
 	Msg  string      `json:"msg"`
 	Data interface{} `json:"data,omitempty"`
 }
 
-func Response(w http.ResponseWriter, resp interface{}, err error) {
+func ErrResponse(w http.ResponseWriter, errMsg ErrorMsg) {
+	body := Body{
+		Code: errMsg.Code,
+		Msg:  errMsg.Msg,
+	}
+	httpx.OkJson(w, body)
+}
+
+func Response(w http.ResponseWriter, resp interface{}, err error, code ...int) {
 	var body Body
 	if err != nil {
-		body.Code = -1
+		if len(code) > 0 {
+			body.Code = code[0]
+		} else {
+			body.Code = -1
+		}
 		body.Msg = err.Error()
 	} else {
 		body.Msg = "OK"
