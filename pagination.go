a669d339368d2a69348f271644962529b92b32dd
lhcGinv <530176577@qq.com>
2024-02-23 21:07:30 +0800

    fix: 修改用于获取sql分页的limit和offset的函数

--- pagination.go
+++ pagination.go
@@ -1,12 +1,13 @@
 package util
 
 // LimitOffset 用于获取sql分页的limit和offset
-func LimitOffset(page, pageSize int64) (limit, offset uint64) {
+func LimitOffset(page, pageSize int32) (limit, offset uint64) {
 	limit = uint64(pageSize)
 	if page < 1 {
 		offset = 1
+		return
 	}
-	offset = uint64((page - 1) * pageSize)
 
+	offset = uint64((page - 1) * pageSize)
 	return
 }
